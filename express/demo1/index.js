e69408b1b4fa03dab8edba1f95e3ab90c7f72977
xiguaaxigua <by_openwater@163.com>
2016-09-01 13:57:15 +0800

    modify package.json

--- express/demo1/index.js
+++ express/demo1/index.js
@@ -1,3 +1,24 @@
 /**
  * Created by zhiyuan.liu on 16/9/1.
  */
+var express = require('express'),
+    app = express();
+
+app.set('port', 1234);
+
+app.use(function (req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl+C to terminate.');
+});
